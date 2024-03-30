d280f9ff2905449d9f32751e6c48b78c67b8fcf4
Bathiya Nethsara <157584911+bathiya2005@users.noreply.github.com>
2024-03-30 14:21:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add Event Listener to Button
+document.getElementById("nextPageButton").addEventListener("click", function() {
+    // Navigate to the next page
+    window.location.href = "nextPage.html";
+});
